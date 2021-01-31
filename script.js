e2c454261692196c766d114c123b81e573f44cd9
Drew Bassett <73472116+drewbassett24@users.noreply.github.com>
2021-01-31 21:32:52 +0000

    renamed js file correctly and debugged. A lot. Got Temperature working.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 //Declare variable of city to search
 var city="";
 //Other variables
@@ -24,7 +25,7 @@ function find(c){
 //API key
 var APIKey="5a13d21d8e32e8262c6690f889c06517";
 //Display current weather and forecast after acquiring city from the input box
-function displayweather(event){
+function displayWeather(event){
     event.preventDefault();
     if(searchCity.val().trim()!==""){
         city=searchCity.val().trim();
@@ -51,10 +52,13 @@ function currentWeather(city){
 
 
         //parse response to display current temperatures
+        var temp = (response.main.temp - 273.15);
+        $(currentTemperature).html((temp).toFixed(2)+"&#8451")
+
         $(currentCity).html(response.name +"("+date+")" + "<img src="+iconurl+">");
         
         // display humidity
-        $(currentHumidty).html(response.main.humidity+"%");
+        $(currentHumidty).html(response.main.humidity+""+"%");
 
         //Windspeed in mph
         var ws=response.wind.speed;
@@ -85,48 +89,91 @@ function currentWeather(city){
     });
 }
         
-        
-        
-        
-        
-        
-        
-        
-        // UVIndex
-
-        //By Geographic coordinates method and using api and coordinates as a parameter build our uv query url inside the function below.
-
-
-
-
         //Function to return the UVIndex response
-
+function UVIndex(ln,lt){
         //Build URL for UVIndex
-
+        var uvqURL="https://api.openweathermap.org/data/2.5/uvi?appid="+ APIKey+"&lat="+lt+"&lon="+ln;
+    $.ajax({
+            url:uvqURL,
+            method:"GET"
+            }).then(function(response){
+                $(currentUvindex).html(response.value);
+            });
+}
 
         //Display 5 day forecast for current city
+function forecast(cityid){
+     var dayover= false;
+     var queryforcastURL="https://api.openweathermap.org/data/2.5/forecast?id="+cityid+"&appid="+APIKey;
+     $.ajax({
+        url:queryforcastURL,
+        method:"GET"
+    }).then(function(response){
+                
+        for (i=0;i<5;i++){
+            var date= new Date((response.list[((i+1)*8)-1].dt)*1000).toLocaleDateString();
+            var iconcode= response.list[((i+1)*8)-1].weather[0].icon;
+            var iconurl="https://openweathermap.org/img/wn/"+iconcode+".png";
+            var tempK= response.list[((i+1)*8)-1].main.temp;
+            var tempF=(((tempK-273.5)*1.80)+32).toFixed(2);
+            var humidity= response.list[((i+1)*8)-1].main.humidity;
+        
+            $("#fDate"+i).html(date);
+            $("#fImg"+i).html("<img src="+iconurl+">");
+            $("#fTemp"+i).html(tempF+"&#8457");
+            $("#fHumidity"+i).html(humidity+"%");
+        }
+        
+    });
+}
 
-
-
-
-
-        //Dynamically add the last city to the search history
-
-
-
-        //Display the last search again when the list group is selected in Search History
-
-
-
-        //Write a function here
-
-
-
-        //Clear Search History
-
-
-
-        //Click handlers
-
-
+//Dynamically add the last city to the search history
+function addToList(c){
+    var listEl= $("<li>"+c.toUpperCase()+"</li>");
+    $(listEl).attr("class","list-group-item");
+    $(listEl).attr("data-value",c.toUpperCase());
+    $(".list-group").append(listEl);
+}
+     
+//Display the last search again when the list group is selected in Search History
+function invokePastSearch(event){
+    var liEl=event.target;
+    if (event.target.matches("li")){
+        city=liEl.textContent.trim();
+        currentWeather(city);
     }
+
+}
+
+//Write a function here
+function loadlastCity(){
+    $("ul").empty();
+    var sCity = JSON.parse(localStorage.getItem("cityname"));
+    if(sCity!==null){
+        sCity=JSON.parse(localStorage.getItem("cityname"));
+        for(i=0; i<sCity.length;i++){
+            addToList(sCity[i]);
+        }
+        city=sCity[i-1];
+        currentWeather(city);
+    }
+
+}
+//Clear Search History
+function clearHistory(event){
+    event.preventDefault();
+    sCity=[];
+    localStorage.removeItem("cityname");
+    document.location.reload();
+}
+        
+//Click Handlers
+$("#search-button").on("click",displayWeather);
+$(document).on("click",invokePastSearch);
+$(window).on("load",loadlastCity);
+$("#clear-history").on("click",clearHistory);
+
+
+        
+
+
